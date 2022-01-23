3260c9930d26b07aa61c257ad96c6236bcf73e3e
DanielPoznanski <35938711+DanielPoznanski@users.noreply.github.com>
2022-01-23 16:53:44 +0100

    Create labki6.swift

---
+++ labki6.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+/*
+//zad 6.1
+var tab: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print ("Podaj wartosc do wstawienia jako pierwszy element")
+var x = Int(readLine()!)
+//polecenie 1
+tab[0] = x!
+//polecenie 2
+print("Podaj na ktorym miejscu w tablicy wpisac element")
+var y = Int(readLine()!)
+if (y! > 10 || y! < 0){
+        tab.append(x!)
+}
+else{
+     tab[y!-1] = x!
+}
+for i in tab{
+             print(i)
+}
+*/
+
+/*
+// zad 6.2
+var tab: [Int] = []
+print ("Podaj ilosc elementow tablicy")
+var x = Int(readLine()!)
+for _ in 1...x!{
+  print ("Podaj wartosc tablicy")
+  let y = Int(readLine()!)
+           tab.append(y!)
+}
+print("Podaj wartosc, ktora chcesz sprawdzic")
+var num = Int(readLine()!)
+if (tab[0] == num!){
+print("Element \(num!) jest pierwszy w tabeli")
+}
+if (tab[x!-1] == num!){
+print("Element \(num!) jest ostatni w tabeli")
+}
+else{
+print("Element nie jest ani pierwszy ani ostatni")
+}
+
+tab.sort()
+print ("Wartosci w tablicy")
+for i in tab{
+             print(i)
+}
+
+print("Najmniejsza wartosc")
+print(tab[0])
+print ("Najwieksza wartosc")
+print(tab[x!-1])
+*/
